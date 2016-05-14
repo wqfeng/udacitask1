ae6f07929efc2a56ab39bcd24e93db2911c4052d
Wang Qun Feng <qunfengw@gmail.com>
2016-05-14 20:35:19 +0800

    1st version to meet the specification

--- todolist.rb
+++ todolist.rb
@@ -1,7 +1,57 @@
 class TodoList
-    # methods and stuff go here
+  # methods and stuff go here
+  attr_accessor :title, :items
+
+  def initialize(title)
+    @title = title
+    @items = Array.new
+  end
+
+  # Creates a new Item and adds it to the array of Items
+  def add_item(new_item)
+    item = Item.new(new_item)
+    @items.push(item)
+  end
+
+  # delete an item with titile
+  def remove_item(id)
+    @items.delete_at(id)
+  end
+
+  # update status
+  def update_status(id, status)
+    @items[id].completed_status = status
+  end
+
+  # it will be called automatically in a to string context
+  def to_s
+  	s = "---------------------\n#{@title}\n---------------------\n"
+    @items.each_with_index {|e, id| s += "#{id + 1} - #{e}\n"}
+
+    return s
+  end
+
+
 end
 
 class Item
-    # methods and stuff go here
+  # methods and stuff go here
+  attr_accessor :description, :completed_status
+
+  # Initialize item with a description and marked as
+  # not complete
+  def initialize(item_description)
+    @description = item_description
+    @completed_status = false
+  end
+
+  def competed?
+    return @completed_status
+  end
+
+  def to_s
+  	@description + " " * (50 - @description.length)  + "Completed: #{@completed_status}"
+
+  end
+
 end
--- udacitask.rb
+++ udacitask.rb
@@ -1,23 +1,36 @@
 require_relative 'todolist.rb'
 
 # Creates a new todo list
+todo_list = TodoList.new("Qun Feng's TODOs")
 
 # Add four new items
+todo_list.add_item("Submit the Udacitask project")
+todo_list.add_item("Heat vs Raptos tomorrow")
+todo_list.add_item("Running for fun!")
 
 # Print the list
+puts todo_list
 
 # Delete the first item
+todo_list.remove_item(0)
 
 # Print the list
+puts todo_list
 
 # Delete the second item
+todo_list.remove_item(0)
 
 # Print the list
+puts todo_list
 
 # Update the completion status of the first item to complete
+todo_list.update_status(0, true)
 
 # Print the list
+puts todo_list
 
 # Update the title of the list
+todo_list.title = "Almost finished!"
 
 # Print the list
+puts todo_list
